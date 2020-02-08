a22018811b7f1554c3af0ee8ee5dc6180a60eef6
Alex-1983 <1983-1111@mail.ru>
2020-02-09 01:17:58 +0500

    review

---
+++ source/js/02__slider.js
@@ -0,0 +1,24 @@
+'use strict';
+
+(function() {
+  var reviews = document.querySelector(".reviews");
+  var reviewsSlider = reviews.querySelector(".swiper-container");
+  var pagination = reviews.querySelector(".swiper-pagination");
+  var buttonPrevious = reviews.querySelector(".slider-toggles__btn--prev");
+  var buttonNext = reviews.querySelector(".slider-toggles__btn--next");
+
+  var promoSwiper = new Swiper(reviewsSlider, {
+    slidesPerView: 1,
+    loop: true,
+    navigation: {
+      nextEl: buttonNext,
+      prevEl: buttonPrevious
+    },
+
+    pagination: {
+      el: pagination,
+      clickable: true,
+    }
+  });
+
+})();
