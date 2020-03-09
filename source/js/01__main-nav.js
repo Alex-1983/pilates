e262ba28ca556d1dff0fbcbafdb0401506e3dd10
Alex-1983 <1983-1111@mail.ru>
2020-03-09 19:47:25 +0500

    final

--- source/js/01__main-nav.js
+++ source/js/01__main-nav.js
@@ -4,6 +4,7 @@
   var openMenuBtn = document.querySelector(".burger");
   var closeMenuBtn = document.querySelector(".close");
   var openMenu = document.querySelector(".main-header__navigation");
+  var links = document.querySelectorAll(".main-nav__link");
   var openMenuClassName = "main-nav-open";
 
   var onOpenMenuBtnClick = function() {
@@ -13,6 +14,11 @@
   var onCloseMenuBtnClick = function() {
     openMenu.classList.remove(openMenuClassName);
   };
+
+  for (var i = 0; i < links.length; i++) {
+    links[i].addEventListener('click', onCloseMenuBtnClick);
+  }
+
 
   openMenuBtn.addEventListener("click", onOpenMenuBtnClick);
   closeMenuBtn.addEventListener("click", onCloseMenuBtnClick);
---
+++ source/js/04__to-top.js
@@ -0,0 +1,52 @@
+'use strict';
+
+(function() {
+
+  var btnTop = document.getElementById('to-top');
+
+  var movementTop = function(e) {
+    e.preventDefault();
+    var scroll = window.pageYOffset;
+    var targetTop = 0;
+    var scrollDiff = (scroll - targetTop) * -1;
+    animate({
+      duration: 500,
+      timing: function(timeFraction) {
+        return Math.pow(timeFraction, 4);
+      },
+      draw: function(progress) {
+        var scrollNow = scroll + progress * scrollDiff;
+        window.scrollTo(0, scrollNow);
+      }
+    });
+  }
+
+  var visibilityToggle = function() {
+    if (window.pageYOffset >= 500) {
+      document.getElementById('to-top').classList.add('to-top--visible');
+    } else {
+      document.getElementById('to-top').classList.remove('to-top--visible');
+    }
+  }
+
+  var animate = function(ref) {
+    var timing = ref.timing,
+      draw = ref.draw,
+      duration = ref.duration;
+    var start = performance.now();
+    requestAnimationFrame(function animate(time) {
+      var timeFraction = (time - start) / duration;
+      if (timeFraction > 1) timeFraction = 1;
+      var progress = timing(timeFraction);
+      draw(progress);
+      if (timeFraction < 1) {
+        requestAnimationFrame(animate);
+      }
+    });
+  }
+
+  btnTop.addEventListener('click', movementTop);
+  window.addEventListener('scroll', visibilityToggle);
+  visibilityToggle();
+
+})();
